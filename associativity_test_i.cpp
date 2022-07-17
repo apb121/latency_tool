732515bc3e674dd7a2f677fc6ec6dc318b177d9c
Alex Brady <apb121@vertex53.doc.ic.ac.uk>
2022-07-17 18:45:41 +0100

    instruction associativity added

---
+++ associativity_test_i.cpp
@@ -0,0 +1,345 @@
+#include <iostream>
+#include <functional>
+#include <cstdlib>
+
+#define NUM_FUNCTIONS 32
+//#define NUM_CALLS 1441440
+#define NUM_CALLS 1680
+
+using namespace std;
+
+void function_0()
+{
+    int i = 0;
+    for (int j = 0; j < 10; j++)
+    {
+        i = j + 1;
+    }
+}
+
+void function_1()
+{
+    int i = 0;
+    for (int j = 0; j < 10; j++)
+    {
+        i = j + 1;
+    }
+}
+
+void function_2()
+{
+    int i = 0;
+    for (int j = 0; j < 10; j++)
+    {
+        i = j + 1;
+    }
+}
+
+void function_3()
+{
+    int i = 0;
+    for (int j = 0; j < 10; j++)
+    {
+        i = j + 1;
+    }
+}
+
+void function_4()
+{
+    int i = 0;
+    for (int j = 0; j < 10; j++)
+    {
+        i = j + 1;
+    }
+}
+
+void function_5()
+{
+    int i = 0;
+    for (int j = 0; j < 10; j++)
+    {
+        i = j + 1;
+    }
+}
+
+void function_6()
+{
+    int i = 0;
+    for (int j = 0; j < 10; j++)
+    {
+        i = j + 1;
+    }
+}
+
+void function_7()
+{
+    int i = 0;
+    for (int j = 0; j < 10; j++)
+    {
+        i = j + 1;
+    }
+}
+
+void function_8()
+{
+    int i = 0;
+    for (int j = 0; j < 10; j++)
+    {
+        i = j + 1;
+    }
+}
+
+void function_9()
+{
+    int i = 0;
+    for (int j = 0; j < 10; j++)
+    {
+        i = j + 1;
+    }
+}
+
+void function_10()
+{
+    int i = 0;
+    for (int j = 0; j < 10; j++)
+    {
+        i = j + 1;
+    }
+}
+
+void function_11()
+{
+    int i = 0;
+    for (int j = 0; j < 10; j++)
+    {
+        i = j + 1;
+    }
+}
+
+void function_12()
+{
+    int i = 0;
+    for (int j = 0; j < 10; j++)
+    {
+        i = j + 1;
+    }
+}
+
+void function_13()
+{
+    int i = 0;
+    for (int j = 0; j < 10; j++)
+    {
+        i = j + 1;
+    }
+}
+
+void function_14()
+{
+    int i = 0;
+    for (int j = 0; j < 10; j++)
+    {
+        i = j + 1;
+    }
+}
+
+void function_15()
+{
+    int i = 0;
+    for (int j = 0; j < 10; j++)
+    {
+        i = j + 1;
+    }
+}
+
+void function_16()
+{
+    int i = 0;
+    for (int j = 0; j < 10; j++)
+    {
+        i = j + 1;
+    }
+}
+
+void function_17()
+{
+    int i = 0;
+    for (int j = 0; j < 10; j++)
+    {
+        i = j + 1;
+    }
+}
+
+void function_18()
+{
+    int i = 0;
+    for (int j = 0; j < 10; j++)
+    {
+        i = j + 1;
+    }
+}
+
+void function_19()
+{
+    int i = 0;
+    for (int j = 0; j < 10; j++)
+    {
+        i = j + 1;
+    }
+}
+
+void function_20()
+{
+    int i = 0;
+    for (int j = 0; j < 10; j++)
+    {
+        i = j + 1;
+    }
+}
+
+void function_21()
+{
+    int i = 0;
+    for (int j = 0; j < 10; j++)
+    {
+        i = j + 1;
+    }
+}
+
+void function_22()
+{
+    int i = 0;
+    for (int j = 0; j < 10; j++)
+    {
+        i = j + 1;
+    }
+}
+
+void function_23()
+{
+    int i = 0;
+    for (int j = 0; j < 10; j++)
+    {
+        i = j + 1;
+    }
+}
+
+void function_24()
+{
+    int i = 0;
+    for (int j = 0; j < 10; j++)
+    {
+        i = j + 1;
+    }
+}
+
+void function_25()
+{
+    int i = 0;
+    for (int j = 0; j < 10; j++)
+    {
+        i = j + 1;
+    }
+}
+
+void function_26()
+{
+    int i = 0;
+    for (int j = 0; j < 10; j++)
+    {
+        i = j + 1;
+    }
+}
+
+void function_27()
+{
+    int i = 0;
+    for (int j = 0; j < 10; j++)
+    {
+        i = j + 1;
+    }
+}
+
+void function_28()
+{
+    int i = 0;
+    for (int j = 0; j < 10; j++)
+    {
+        i = j + 1;
+    }
+}
+
+void function_29()
+{
+    int i = 0;
+    for (int j = 0; j < 10; j++)
+    {
+        i = j + 1;
+    }
+}
+
+void function_30()
+{
+    int i = 0;
+    for (int j = 0; j < 10; j++)
+    {
+        i = j + 1;
+    }
+}
+
+void function_31()
+{
+    int i = 0;
+    for (int j = 0; j < 10; j++)
+    {
+        i = j + 1;
+    }
+}
+
+int main(int argc, char** argv)
+{
+    int num_functions = atoi(argv[1]);
+    function<void()> f[32]
+    {
+        function_0,
+        function_1,
+        function_2,
+        function_3,
+        function_4,
+        function_5,
+        function_6,
+        function_7,
+        function_8,
+        function_9,
+        function_10,
+        function_11,
+        function_12,
+        function_13,
+        function_14,
+        function_15,
+        function_16,
+        function_17,
+        function_18,
+        function_19,
+        function_20,
+        function_21,
+        function_22,
+        function_23,
+        function_24,
+        function_25,
+        function_26,
+        function_27,
+        function_28,
+        function_29,
+        function_30,
+        function_31
+    };
+    for (int i = 0; i < NUM_CALLS / num_functions; ++i)
+    {
+        for (int j = 0; j < num_functions; ++j)
+        {
+            f[j]();
+        }
+    }
+    return 0;
+}
--- system_test.cpp
+++ system_test.cpp
@@ -4,6 +4,7 @@
 #include <cstring>
 #include <map>
 #include <unistd.h>
+#include <fstream>
 
 using namespace std;
 
@@ -56,6 +57,48 @@ int main()
 		   sysconf(_SC_LEVEL4_CACHE_SIZE),
 		   sysconf(_SC_LEVEL4_CACHE_LINESIZE),
 		   sysconf(_SC_LEVEL4_CACHE_ASSOC));
-    
+
+    char buf[65536];
+    string cmd_compile = "g++ -g -falign-functions=65536 ./associativity_test_i.cpp -o ./associativity_test_i";
+    string cmd_no_arg = "perf stat -x , --append -o assoctmp.txt -e L1-icache-load-misses -r 1000 ./associativity_test_i ";
+    string cmd_rm_assoctmp = "rm ./assoctmp.txt";
+    int ret_compile = system(cmd_compile.c_str());
+    if (ret_compile)
+    {
+      cout << "Compilation failed!" << endl;
+    }
+    int ret_rm_assoctmp = system(cmd_rm_assoctmp.c_str());
+    for (int i = 2; i <= 16; i += 2)
+    {
+      string cmd_full = cmd_no_arg + to_string(i);
+      FILE* cmd_stream;
+      cmd_stream = popen(cmd_full.c_str(), "r");
+      pclose(cmd_stream);
+    }
+    ifstream assoc_file;
+    assoc_file.open("./assoctmp.txt");
+    string atstr;
+    int assoc = -1;
+    int assoc_arr[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+    int i = 0;
+    while (getline(assoc_file, atstr))
+    {
+      if (atstr.size() == 0 || atstr[0] == '#')
+      {
+        continue;
+      }
+      size_t found = atstr.find_first_not_of("0123456789");
+      if (found == 0)
+      {
+        continue;
+      }
+      assoc_arr[i] = stoi(atstr.substr(0, found));
+      if (i > 1 && assoc_arr[i] - assoc_arr[i - 1] < assoc_arr[i - 1] - assoc_arr[i - 2])
+      {
+        cout << "Suggested associativity: " << ((i - 1) + 1) * 2 << endl;
+        break;
+      }
+      i++;
+    }
     return 0;
 }
